0439c0a92d5a1c648b3c8657f98593747324629a
muhammadnomad <hcb9br@virginia.edu>
2025-06-23 06:34:16 +0500

    feat: update AppNavigator to include Login, Signup, and OTP Verification screens

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -4,13 +4,16 @@ import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen';
 import ItemDetailScreen from '../screens/ItemDetailScreen';
 import AddItemScreen from '../screens/AddItemScreen';
+import LoginScreen from '../screens/LoginScreen';
+import SignupScreen from '../screens/SignupScreen';
+import OTPVerificationScreen from '../screens/OTPVerificationScreen';
 
 const Stack = createStackNavigator();
 
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen 
           name="Home" 
           component={HomeScreen} 
@@ -25,6 +28,21 @@ export default function AppNavigator() {
           name="Add" 
           component={AddItemScreen} 
           options={{ title: 'Post an Item' }} 
+        />
+        <Stack.Screen 
+          name="Login" 
+          component={LoginScreen} 
+          options={{ title: 'Login' }} 
+        />
+        <Stack.Screen 
+          name="Signup" 
+          component={SignupScreen} 
+          options={{ title: 'Sign Up' }} 
+        />
+        <Stack.Screen 
+          name="OTPVerification" 
+          component={OTPVerificationScreen} 
+          options={{ title: 'Verify Email' }} 
         />
       </Stack.Navigator>
     </NavigationContainer>
